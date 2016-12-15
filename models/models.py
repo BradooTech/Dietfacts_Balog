a60fec54b7a01598bb530a2caea71fe767c74dc5
dev <gabriel.balog@bradootech.com>
2016-12-15 15:57:40 -0200

    Inclusão de Menu Items e criação de Model

--- models/models.py
+++ models/models.py
@@ -9,14 +9,15 @@ class Dietfacts_product_template(models.Model):
 	calories = fields.Integer('Calories')
 	servingsize = fields.Float('Serving Size')
 	lastupdate = fields.Date('Last Update')
+	#dietitem = fields.Boolean("Diet Item")
 
-
-
-
-
-
-
-
+class DietFacts_res_users_meal(models.Model):
+	_name = 'res.users.meal'
+	name = fields.Char('Meal Name')
+	meal_date = fields.Datetime('Menu Date')
+	#item_ids = fields.One2many()
+	user_id = fields.Many2one('res.users','Meal User')
+	notes = fields.Text('Meal Notes')
 
 
 # class dietfacts(models.Model):
@@ -24,7 +25,7 @@ class Dietfacts_product_template(models.Model):
 
 #     name = fields.Char()
 #     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
+#     value2 = fields.Float(compute='_value_pc', store=True)
 #     description = fields.Text()
 #
 #     @api.depends('value')
